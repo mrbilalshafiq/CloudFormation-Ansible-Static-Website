7d04262f0426790e14dbdd2bf59970be556cf42e
Bilal Shafiq <75343545+mrbilalshafiq@users.noreply.github.com>
2022-03-24 15:29:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # install ansible, python 3 & pip
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y ansible python3 python3-pip
+sudo apt install -y ansible python3 python3-pip awscli
 # install awscli and boto3 dependencies
 pip3 install --user --upgrade boto3 awscli
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
